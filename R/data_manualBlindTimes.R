babad4c288bd2f3cbbf26e448e1def47ffa0be0b
Birgen Haest <birgen.haest@vogelwarte.ch>
2023-02-17 08:50:06 +0000

    version 0.1.2

---
+++ R/data_manualBlindTimes.R
@@ -0,0 +1,16 @@
+#' Example file on how to include manual blind times for your 'Birdscan MR1' 
+#' database. 
+#'
+#' To create your own manual blind times file, just copy this file, and adjust.
+#'
+#' @docType data
+#'
+#' @usage data(manualBlindTimes)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(manualBlindTimes)
+"manualBlindTimes"
